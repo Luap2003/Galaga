3401be4e50c20b2338efe6b4a0cc26b8b72f8946
Luap2003 <luapglaser2@gmail.com>
2024-01-30 09:33:48 +0100

    new Movement logic for enemys

--- src/main/java/views/Model.java
+++ src/main/java/views/Model.java
@@ -24,7 +24,7 @@ public class Model {
         player = Player.getInstance((int) map.length/2, (int) map[0].length, 0, 0, 100,100);
         gameObjects.add(player);
         gameObjects.add(new Enemy((int) map.length/2, 0, 0, 0, 10, 0, 100, 100));
-        gameObjects.add(new Asteroid(200, 200, 0, 1, 10, 100, 100));
+        //gameObjects.add(new Asteroid(200, 200, 0, 1, 10, 100, 100));
 
 
         // Schedule a new TimerTask to run every second
@@ -109,10 +109,10 @@ public class Model {
     public void spawnEnemies() {
         Random random = new Random();
         if (random.nextInt(200) < 1) {
-            int x = random.nextInt(2000); // Random x-coordinate
+            int x = 800+random.nextInt(400); // Random x-coordinate
             int y = 0; // Top of the screen
             int dx = random.nextInt(2) * 2 - 1; // Random horizontal speed between -1 and 1
-            int dy = 1; // Random vertical speed between 1 and 3
+            int dy = 2; // Random vertical speed between 1 and 3
             int lives = random.nextInt(30) + 1; // Random number of lives between 1 and 3
             Enemy enemy = new Enemy(x, y, dx, dy, 1, lives,100,100 ); // Create new enemy
             gameObjects.add(enemy); // Add enemy to game objects
